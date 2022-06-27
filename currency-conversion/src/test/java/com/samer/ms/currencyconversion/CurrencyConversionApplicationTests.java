ae787e960301b51dc3fe388745fbfc9c69a69483
samergosain <samergosain@localhost>
2022-06-28 03:18:07 +0530

    Currency Conversion microservice

---
+++ currency-conversion/src/test/java/com/samer/ms/currencyconversion/CurrencyConversionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samer.ms.currencyconversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
